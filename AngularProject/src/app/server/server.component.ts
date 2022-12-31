2e3edeeb26017176d717260c97fe473634343401
nullgar <technicianaguilar@gmail.com>
2022-12-30 23:20:56 -0800

    Learned about decorators, components and how to create custom ones

---
+++ AngularProject/src/app/server/server.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+    
+}
+
+
+// const ServerComponent = () => { //This will not work - This is React Syntax
+
+// };
+
+// export default ServerComponent;
