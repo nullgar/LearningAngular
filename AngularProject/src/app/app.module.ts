22b5eca5a601c9e96d8c6567a869c9d40da2eb76
nullgar <technicianaguilar@gmail.com>
2023-01-01 18:51:19 -0800

    Create a warning alert component

--- AngularProject/src/app/app.module.ts
+++ AngularProject/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { MerryChristmasComponent } from './merry-christmas/merry-christmas.component';
+import { WarningAlert } from './warning-alert/warning-alert.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    MerryChristmasComponent
+    MerryChristmasComponent,
+    WarningAlert
   ],
   imports: [
     BrowserModule
